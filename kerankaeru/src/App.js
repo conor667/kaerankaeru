e8562f9df4ec2ea020558adb6069a55886b550a8
Conor White <conorwhite667@gmail.com>
2022-10-12 15:03:09 +0100

    added switch statement to App.js to allow changing pages

--- kerankaeru/src/App.js
+++ kerankaeru/src/App.js
@@ -1,12 +1,32 @@
 import './App.css';
 import Home from "./components/pages/Home";
 import Navbar from './components/navbar/Navbar';
+import About from './components/pages/About';
+import Upcoming from './components/pages/Upcoming';
 
 function App() {
+
+  let Component;
+
+	//Switch statement to change web pages
+	switch (window.location.pathname) {
+		case "/Home":
+			Component = Home;
+			break;
+		case "/About":
+			Component = About;
+			break;
+    case "/Upcoming":
+      Component = Upcoming;
+      break;
+		default:
+			Component = Home;
+			break;
+  }
   return (
     <div>
       <Navbar/>
-      <Home/>
+      <Component/>
     </div>
   );
 }
