2e02ffb840509c8e20353ae00b8ab90b6068b3aa
Conor White <conorwhite667@gmail.com>
2022-10-12 15:03:47 +0100

    made basic contact page

---
+++ kerankaeru/src/components/pages/Contact.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Contact(){
+    return(
+        <div>
+            <h1 className="Bandtitle">Kaerankaeru</h1>
+        </div>
+    );
+};
+export default Contact;
