8b9ed59fb36ebceb284d31b2b519c1cbd9f7f1ab
Conor White <conorwhite667@gmail.com>
2022-10-12 15:55:22 +0100

    added formatting to the different names, tried to add icons for each instrument they play but can't figure out why it isn't being added

--- kerankaeru/src/components/pages/Home.js
+++ kerankaeru/src/components/pages/Home.js
@@ -1,17 +1,18 @@
 import React from "react";
-import './Home.css'
+import './Home.css';
+import {FontAwesomeIcon} from '@fortawesome/fontawesome-free';
 
 function Home(){
     return(
-        <div>
+        <div className="homepage">
             <h1 className="title">Kaerankaeru</h1>
             <img className="bandlogoglow" src={require("./logowithglow.png")}></img>
-            <ul>
-                <li> Aaron </li>
-                <li> Frank </li>
-                <li> Akane </li>
-                <li> Gideon </li>
-                <li> Truman </li>
+            <ul className="bandmembernames">
+                <ol> Aaron </ol>
+                <ol> Frank </ol>
+                <ol> Akane </ol>
+                <ol> Gideon </ol>
+                <ol> Truman </ol>
             </ul>
         </div>
     );
