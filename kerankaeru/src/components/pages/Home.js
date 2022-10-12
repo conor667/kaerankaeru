7e11832f35755e0cfedfe6452f34b9cc66e75cf0
Conor White <conorwhite667@gmail.com>
2022-10-12 14:27:06 +0100

    made react project

---
+++ kerankaeru/src/components/pages/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './logo.jpg'
+
+function Home(){
+    return(
+        <div>
+            <h1>Kaerankaeru</h1>
+            <img src="logo.png"></img>
+        </div>
+    );
+};
+export default Home;
