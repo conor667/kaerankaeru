a2e3ace624ea95fa5ae7afb301db3672b0b0782a
Conor White <conorwhite667@gmail.com>
2022-10-12 15:32:56 +0100

    updated Upcoming page

--- kerankaeru/src/components/pages/Upcoming.js
+++ kerankaeru/src/components/pages/Upcoming.js
@@ -4,6 +4,15 @@ function Upcoming(){
     return(
         <div>
             <h1 className="Bandtitle">Upcoming</h1>
+            <p>We'll be performing at @firecafeosaka on the 15th of October alongside OS Vagalumes!
+                Come and check it out, @kaerankaeru will play from 9 PM!
+
+                Entry is free!
+
+                Details:
+                Fire Cafe
+                2F 2 Chome-9-36 Nishishinsaibashi, Chuo Ward, Osaka, 542-0086
+</p>
         </div>
     );
 };
